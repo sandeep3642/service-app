e19907374f775af11e1396d4cba68cb5d3a6de16
Abhishek Negi <abhishek.negi@rapidsoft.co.in>
2025-06-23 18:32:23 +0530

    UI changes for service activity

--- src/pages/Service-Request-Manager/ActivityLog.jsx
+++ src/pages/Service-Request-Manager/ActivityLog.jsx
@@ -1,76 +1,54 @@
 import React from "react";
-import Icon from "../../assets/icon.png";
+// import Icon from "../../assets/icon.png";
+import { getMessageName } from "../../utilty/messageConstant";
+import { formatDate } from "../../utilty/common";
 
-const timelineData = [
-  {
-    title: "Service Requested by Customer",
-    time: "02 May 2025 – 10:00 AM",
-  },
-  {
-    title: "Technician Assigned: Ramesh J.",
-    time: "02 May 2025 – 11:30 AM",
-  },
-  {
-    title: "Technician Arrived at Customer Location",
-    time: "03 May 2025 – 01:20 PM",
-  },
-  {
-    title: "Part Requested: Cooling Fan",
-    time: "03 May 2025 – 02:45 PM",
-  },
-  {
-    title: "Quotation Sent to Customer",
-    time: "04 May 2025 – 12:10 PM",
-  },
-  {
-    title: "Payment Received: ₹5,100",
-    time: "04 May 2025 – 12:30 PM",
-  },
-  {
-    title: "Part Delivered",
-    time: "05 May 2025 – 04:00 PM",
-  },
-  {
-    title: "Service Completed",
-    time: "06 May 2025 – 10:30 AM",
-  },
-];
+const ActivityLog = ({ timelineData }) => {
+  const sortedActivities =timelineData && timelineData.sort(
+    (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+  );
 
-const ActivityLog = () => {
   return (
-    <div className="p-6">
+    <div className="p-2  w-screen">
       {/* Header */}
-      <h2 className="text-xl font-semibold text-[#393939] mb-3">
-        Activity Log
+      <h2 className="text-xl font-semibold text-[#393939] mb-0">
+        Service Activity Log
       </h2>
-      <div className="text-sm text-gray-600 mb-4">
-        Service ID: <span className="font-medium text-black">#SR123456</span>
+      <div className="text-sm text-gray-600 mb-0">
+        {/* Service ID: <span className="font-medium text-black">#SR123456</span> */}
       </div>
-      <div className="text-sm text-gray-600 mb-4  w-180 flex justify-between">
-        <span>
+      <div className="text-sm text-gray-600 mb-0  w-180 flex justify-between">
+        {/* <span>
           Order date:{" "}
           <span className="font-medium text-black">02 May 2025</span>
-        </span>
-        <span className="text-[#0C94D2] text-[16px] flex gap-5">
+        </span> */}
+        {/* <span className="text-[#0C94D2] text-[16px] flex gap-5">
           <img src={Icon} alt="icon" className="h-5 w-5" />
           Estimated delivery: May 06, 2025
-        </span>
+        </span> */}
       </div>
 
       {/* Timeline */}
-      <div className="relative border-l-2 border-dashed border-[#0096FF] pl-6 space-y-6 mt-10">
-        {timelineData.map((item, index) => (
-          <div key={index} className="relative">
-            {/* Blue Dot */}
-            <div className="absolute -left-[33px] top-0 w-4 h-4 bg-[#0096FF] rounded-full border border-white"></div>
+      <div className="relative border-l-2 border-dashed border-[#0096FF] pl-6 space-y-6 mt-5 ml-5">
+        {sortedActivities &&
+          sortedActivities.length > 0 &&
+          sortedActivities.reverse().map((item, index) => (
+            <div key={index} className="relative">
+              {/* Blue Dot */}
+              <div className="absolute -left-[33px] top-0 w-4 h-4 bg-[#0096FF] rounded-full border border-white"></div>
 
-            {/* Text Content */}
-            <div>
-              <p className="text-gray-900 font-medium text-lg">{item.title}</p>
-              <p className="text-sm text-[#606060]">{item.time}</p>
+              {/* Text Content */}
+              <div>
+                <p className="text-gray-900 font-medium text-lg">
+                  {getMessageName(item.activityType)}
+                </p>
+                <p className="text-sm text-black">{item.notes}</p>
+                <p className="text-sm text-[#606060]">
+                  {formatDate(item.createdAt,true)}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
